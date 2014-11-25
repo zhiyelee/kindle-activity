86353be6803a472429c59552a6c7c7f5ec917e5b
zhiyelee <zhiyelee@gmail.com>
2014-11-26 00:51:57 +0800

    reformat code

--- index.js
+++ index.js
@@ -3,92 +3,95 @@ var entryList = [];
 var Spooky = require('spooky');
 
 var spooky = new Spooky({
-    child: {
-        transport: 'http'
-    },
-    casper: {
-        logLevel: 'debug',
-        verbose: true,
-        clientScripts: ["jquery.min.js"]
-    }
+  child: {
+    transport: 'http'
+  },
+  casper: {
+    logLevel: 'debug',
+    verbose: true,
+    clientScripts: ["jquery.min.js"]
+  }
 }, function (err) {
-    if (err) {
-        console.log(err)
-    }
-    spooky.start('https://kindle.amazon.com/profile/zhiyelee/11533613');
+  if (err) {
+    console.log(err)
+  }
+  spooky.start('https://kindle.amazon.com/profile/zhiyelee/11533613');
 
-    spooky.then(function () {
-        entryList = this.evaluate(function () {
-            var arr = [];
+  spooky.then(function () {
+    entryList = this.evaluate(function () {
+      var arr = [];
 
-            $('.recentActivityEntry').each(function () {
-                var id = $(this).attr('id');
-                var info = [];
-                info.push(id);
+      $('.recentActivityEntry').each(function () {
+        var id = $(this).attr('id');
+        var info = [];
+        info.push(id);
 
-                var href = $('a:first-child', this).attr('href')
-                info.push(href);
+        var href = $('a:first-child', this).attr('href')
+        info.push(href);
 
-                var ndContent = $('.sampleHighlight', this);
-                var content = $('div:first-child', ndContent).text();
-                info.push(content);
-                var note = $('.sampleNote', ndContent).text();
-                info.push(note);
+        var ndContent = $('.sampleHighlight', this);
+        var content = $('div:first-child', ndContent).text();
+        info.push(content);
+        var note = $('.sampleNote', ndContent).text();
+        info.push(note);
 
-                arr.push(info);
-            });
+        arr.push(info);
+      });
 
-            return arr
-        });
-        this.capture('google.png', undefined, {
-            quality: 75
-        });
+      return arr
     });
-
-
-    spooky.run(function () {
-        var instance = this;
-        entryList.forEach(function (e) {
-            instance.emit('data', e)
-        });
-        instance.exit(0);
+    var instance = this;
+    entryList.forEach(function (e) {
+      instance.emit('data', e)
     });
+  });
+
+
+  spooky.then(function () {
+    this.emit('exit');
+  });
+  spooky.run();
 
 });
 spooky.on('error', function (e, stack) {
-    console.error(e);
+  console.error(e);
 
-    if (stack) {
-        console.log(stack);
-    }
+  if (stack) {
+    console.log(stack);
+  }
 });
 
 spooky.on('data', function (data) {
-    save2db(data)
+  save2db(data);
+  console.log('destro');
+});
+spooky.on('exit', function () {
+  console.log('process exited');
+  process.exit(0)
 });
 
 
 function save2db(e) {
-    var activity = new ActivityModel({
-        uid: e[0],
-        url: e[1],
-        content: e[2],
-        note: e[3]
-    });
+  var activity = new ActivityModel({
+    uid: e[0],
+    url: e[1],
+    content: e[2],
+    note: e[3]
+  });
 
-    // is this needed? mongodb objectid
-    // The ObjectId is generated based on timestamp, machine ID, process ID, and a process-local incremental counter.
-    ActivityModel.findOne({uid: e[0]}, function (err, item) {
-        if (err) {
-            console.log(err)
-            return;
-        }
-        if (!item) {
-            activity.save(function (err) {
-                if (err) return console.error(err);
-                console.log('item inserted.')
-            });
-        }
-    });
+  // is this needed? mongodb objectid
+  // The ObjectId is generated based on timestamp, machine ID, process ID, and a process-local incremental counter.
+  ActivityModel.findOne({uid: e[0]}, function (err, item) {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    if (!item) {
+      activity.save(function (err, item) {
+        if (err) return console.error(err);
+        console.log('item `' + item.uid + '` is inserted.')
+      });
+    }
+  });
 }
 
