b9db7e94260189b9df8dfc12c36b4640642326be
zhiyelee <zhiyelee@gmail.com>
2014-11-27 21:23:13 +0800

    use exec and temporary file to interact with casperjs

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var entryList = [];
 var util = require('./lib/util');
+var exec = require('child_process').exec;
+var fs = require('fs');
 
 var url = 'https://kindle.amazon.com/profile/zhiyelee/11533613';
 
+// TODO get the uid of the lastest activity and pass to the casper
+var cmd = 'casperjs lib/casper.js '
+    + ' --url=' + url;
+
+exec(cmd, function (err) {
+  if (err) console.log(err);
+
+  var entryList = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+  util.save2db(entryList);
+});
