7dbff218d90efa426f224cffc6913124cd592a70
zhiyelee <zhiyelee@gmail.com>
2014-11-28 23:58:17 +0800

    latest activity

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 
 var url = 'https://kindle.amazon.com/profile/zhiyelee/11533613';
+var lastestItem = util.getLatestItem();
 
-// TODO get the uid of the lastest activity and pass to the casper
-var cmd = 'casperjs lib/casper.js '
-    + ' --url=' + url;
+lastestItem.then(function (cid) {
 
-exec(cmd, function (err) {
-  if (err) console.log(err);
+  var cmd = 'casperjs lib/casper.js '
+      + ' --url=' + url;
+  if (uid) {
+    cmd += ' --cid=' + cid;
+  }
 
-  var entryList = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-  util.save2db(entryList);
+  exec(cmd, function (err) {
+    if (err) console.log(err);
+
+    var entryList = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    util.save2db(entryList);
+  });
 });
