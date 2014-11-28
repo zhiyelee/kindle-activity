484eb9a6304a76fb84ad92016f6d61c7633e6082
zhiyelee <zhiyelee@gmail.com>
2014-11-29 01:35:46 +0800

    fix

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ var fs = require('fs');
 var url = 'https://kindle.amazon.com/profile/zhiyelee/11533613';
 var lastestItem = util.getLatestItem();
 
-lastestItem.then(function (cid) {
+lastestItem.then(function (item) {
 
   var cmd = 'casperjs lib/casper.js '
       + ' --url=' + url;
-  if (uid) {
-    cmd += ' --cid=' + cid;
+  if (item && item['uid']) {
+    cmd += ' --cid=' + item['uid'];
+    console.log('Now the latest item is: ' + item['uid']);
   }
 
+  fs.writeFileSync('data.json','');
   exec(cmd, function (err) {
     if (err) console.log(err);
 
-    var entryList = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    var data = fs.readFileSync('data.json', 'utf8');
+
+    if(!data) {
+      process.exit(0);
+    }
+
+    var entryList = JSON.parse(data);
     util.save2db(entryList);
   });
 });
