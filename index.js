777a5b36a724413676a11f936eb3d6c6bb3a628e
zhiyelee <zhiyelee@gmail.com>
2014-11-29 23:55:28 +0800

    fix db disconnect

--- index.js
+++ index.js
@@ -2,29 +2,30 @@ var util = require('./lib/util');
 var exec = require('child_process').exec;
 var fs = require('fs');
 
+var dataFile = 'data.json';
 var url = 'https://kindle.amazon.com/profile/zhiyelee/11533613';
 var lastestItem = util.getLatestItem();
 
 lastestItem.then(function (item) {
 
-  var cmd = 'casperjs lib/casper.js '
-      + ' --url=' + url;
+  var cmd = 'casperjs lib/casper.js ' + ' --url=' + url;
   if (item && item['uid']) {
     cmd += ' --cid=' + item['uid'];
     console.log('Now the latest item is: ' + item['uid']);
   }
 
-  fs.writeFileSync('data.json','');
+  fs.writeFileSync(dataFile, '');
+
   exec(cmd, function (err) {
     if (err) console.log(err);
 
-    var data = fs.readFileSync('data.json', 'utf8');
+    var data = fs.readFileSync(dataFile, 'utf8');
 
-    if(!data) {
-      process.exit(0);
+    if(data) {
+      var entryList = JSON.parse(data);
+      util.save2db(entryList);
     }
 
-    var entryList = JSON.parse(data);
-    util.save2db(entryList);
+    util.disconnectDB()
   });
 });
