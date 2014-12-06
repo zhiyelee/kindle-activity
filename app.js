7eabc5345e9a7a19f5c2f6788ba47f25ebce5069
zhiyelee <zhiyelee@gmail.com>
2014-12-06 20:09:29 +0800

    add single page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var koa = require('koa');
 var kr = require('koa-route');
 var app = koa();
 var util = require('./lib/util');
+var views = require('./lib/views');
+var serveStatic = require('koa-static');
 
 // x-response-time
 app.use(function *(next) {
@@ -18,17 +20,26 @@ app.use(function *(next) {
   var ms = new Date - start;
   console.log('%s %s - %s', this.method, this.url, ms);
 });
+app.use(serveStatic('static'));
 app.use(kr.get('/', function *() {
   this.body = 'index';
 }));
 app.use(kr.get('/:id', function *(id) {
-  var bd = yield util.getActivity(id);
-  if (typeof bd === 'object') {
-    content = bd.content;
-  } else {
-    content = bd;
+  var item = yield util.getActivity(id);
+  // TODO 404 page
+  if (typeof item !== 'object') {
+    this.body = item;
+    return;
   }
-  this.body = content;
+
+  // render html
+  var res = yield views('layout', {
+    title: id,
+    item: item
+  });
+
+  this.type = 'html';
+  this.body = res;
 }));
 
 app.listen(3000);
