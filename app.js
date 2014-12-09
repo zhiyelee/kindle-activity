1b9a276ff4896f4c2e243aaa802cd0a65f3338a6
zhiyelee <zhiyelee@gmail.com>
2014-12-10 00:37:40 +0800

    code refactor && and pagenavbar

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var koa = require('koa');
-var kr = require('koa-route');
 var app = koa();
-var util = require('./lib/util');
-var views = require('./lib/views');
-var serveStatic = require('koa-static');
+var router = require('./lib/router');
 
 // x-response-time
 app.use(function *(next) {
@@ -12,7 +9,6 @@ app.use(function *(next) {
   var ms = new Date - start;
   this.set('X-Response-Time', ms + 'ms');
 });
-
 // logger
 app.use(function *(next) {
   var start = new Date;
@@ -20,59 +16,6 @@ app.use(function *(next) {
   var ms = new Date - start;
   console.log('%s %s - %s', this.method, this.url, ms);
 });
-app.use(serveStatic('static'));
-
-// index
-app.use(kr.get('/', function *() {
-  var items = yield util.getItems();
-  // TODO 404 page
-  if (typeof items !== 'object') {
-    this.body = items;
-    return;
-  }
-
-  // render html
-  var res = yield views('index', {
-    items: util.tailorItems(items, 150)
-  });
-  this.body = res;
-}));
-
-// page page
-app.use(kr.get('/page/:num', function *(num) {
-  if (!/^\d*$/.test(num)) return;
-
-  num = parseInt(num, 10);
-  var items = yield util.getItems(num);
-  if (typeof items !== 'object') {
-    this.body = items;
-    return;
-  }
-  // TODO /page/2
-  // render html
-  var res = yield views('index', {
-    items: util.tailorItems(items, 150)
-  });
-  this.body = res;
-}));
-
-// item page
-app.use(kr.get('/:id', function *(id) {
-  var item = yield util.getItem(id);
-  // TODO 404 page
-  if (typeof item !== 'object') {
-    this.body = item;
-    return;
-  }
-
-  // render html
-  var res = yield views('item', {
-    title: id,
-    item: item
-  });
-
-  this.type = 'html';
-  this.body = res;
-}));
+router.init(app);
 
 app.listen(3000);
