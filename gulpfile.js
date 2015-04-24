7203cfe9a7d1711a09755473f27102de7d7c403e
zhiyelee <zhiyelee@gmail.com>
2015-04-24 16:10:39 -0500

    update versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp');
-var minifyCSS = require('gulp-minify-css');
-var stylus = require('gulp-stylus');
-var nib = require('nib');
+var gulp = require('gulp')
+  , minifyCSS = require('gulp-minify-css')
+  , stylus = require('gulp-stylus')
+  , nib = require('nib');
 
 
 // stylus
 gulp.task('stylus', function () {
-    gulp.src('static/stylus/*.styl')
-        .pipe(stylus({error: true, use: [nib()]}))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('./static/css/'));
+  gulp.src('static/stylus/*.styl')
+    .pipe(stylus({error: true, use: [nib()]}))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./static/css/'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('static/stylus/*.styl', ['stylus']);
+  gulp.watch('static/stylus/*.styl', ['stylus']);
 });
 
 gulp.task('default', ['stylus', 'watch']);
