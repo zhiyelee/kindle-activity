e099e5caa1330dd0b78f7a054d91df6351d9d4aa
zhiyelee <zhiyelee@gmail.com>
2015-05-23 22:27:54 -0500

    render returned data

--- static/js/base.js
+++ static/js/base.js
@@ -5,11 +5,32 @@
 $(document).ready(function () {
   var ndForm = $('#s');
   ndForm.submit(function (e) {
-    var key = $('.search').val();
+    var key = $('.search').val()
+      , ndItemList = $('.item-list');
     $.get('/search', { key: key}, function (data) {
-      console.log(data)
+      if (data.length === 0) {
+        ndItemList.html('<div class="center-block no-result"> No items MATCHED. </div>');
+      } else {
+        ndItemList.html(render(data))
+      }
     });
     e.preventDefault();
-  })
+  });
+
+  function render(items) {
+    var html = [];
+
+    for (var i = 0; i < items.length; i++) {
+      var tpl = ''
+        , item = items[i];
+      tpl = '<article class="item">' +
+              '<div class="item-content">' + item.content + '</div>' +
+              '<footer> <a class="link" href="/' + item.url + '"> >></a> </footer>' +
+            '</article>';
+      html.push(tpl)
+    }
+
+    return html.join('');
+  }
 
 });
